7e6967252abbe1e2d9e8bb170b383ce61a5c8257
wjhstd <lanouhn@wangjunhui.local>
2017-09-20 11:29:32 +0800

    更改结构

--- src/main/java/com/lanou/cn/controller/ArtificialBeforeController.java
+++ src/main/java/com/lanou/cn/controller/ArtificialBeforeController.java
@@ -1,4 +1,13 @@
 package com.lanou.cn.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("cst")
+@Controller
 public class ArtificialBeforeController {
+    @RequestMapping("showCst")
+    public String showCst(){
+        return "/cstservice/cstList";
+    }
 }
